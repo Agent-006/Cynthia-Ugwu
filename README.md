9238e09b086ecc4bacc0d82ab3531a48c34f053c
Sagar~ <sagargope2001@gmail.com>
2024-01-05 18:31:23 +0530

    animated cursor and hero section

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-This is a clone of an awarded website Cynthis Ugwu\
+This is a clone of an awarded website Cynthis Ugwu
 
 📌 Original Website
 🔗 https://cynthiaugwu.com/
@@ -6,21 +6,24 @@ This is a clone of an awarded website Cynthis Ugwu\
 📌 My Work
 🔗 https://cynthia-ugwu-agent-006.vercel.app/
 
+
+
+
 ⚙️ Technologies used 🌟
 
-1️⃣ HTML5\
+1️⃣ HTML5
 
-2️⃣ CSS3\
+2️⃣ CSS3
 
-3️⃣ JavaScript\
+3️⃣ JavaScript
 
-4️⃣ Locomitive\
+4️⃣ Locomitive
 
  🪜Step 1: attach loco scroll css ✅\
  🔗 https://cdn.jsdelivr.net/npm/locomotive-scroll@3.5.4/dist/locomotive-scroll.css \
  🪜Step 2: attach locomotive scroll min.js ✅\
 🔗 https://cdn.jsdelivr.net/npm/locomotive-scroll@3.5.4/dist/locomotive-scroll.js \
- 🪜Step 3: some code from loco github for js ✅\
+ 🪜Step 3: some code from loco github for js ✅
 
     const scroll = new LocomotiveScroll({
     el: document.querySelector('[data-scroll-container]'),
--- script.js
+++ script.js
@@ -2,3 +2,79 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
+
+let timeout;
+
+function skewCursorEffect() {
+
+    let xscale = 1;
+    let yscale = 1;
+
+    let xprev = 0;
+    let yprev = 0;
+
+    window.addEventListener("mousemove", function (dets) {
+        clearTimeout(timeout);
+
+        let xdiff = dets.clientX - xprev;
+        let ydiff = dets.clientX - yprev;
+
+        xscale = gsap.utils.clamp(.8, 1.2, xdiff);
+        yscale = gsap.utils.clamp(.6, 1.2, ydiff);
+
+        xprev = dets.clientX;
+        yprev = dets.clientY;
+
+        customCursor(xscale, yscale);
+        timeout = setTimeout(function () {
+            let crsr = document.querySelector('#cursor');
+
+            crsr.style.transform = `translate(${dets.clientX - 5}px, ${dets.clientY - 5}px) scale(1, 1)`;
+        }, 100);
+    })
+}
+
+skewCursorEffect();
+
+function customCursor(xscale, yscale) {
+    window.addEventListener("mousemove", function (dets) {
+        let crsr = document.querySelector('#cursor');
+
+        crsr.style.transform = `translate(${dets.clientX}px, ${dets.clientY}px) scale(${xscale}, ${yscale})`;
+    })
+}
+
+
+(function animate() {
+    let tl = gsap.timeline();
+
+    tl.from('#nav', {
+        y: '-10',
+        opacity: 0,
+        duration: 1.5,
+        ease: Expo.easeInOut,
+    })
+
+    tl.to('.boundingelemUp', {
+        y: 0,
+        ease: Expo.easeInOut,
+        duration: 1.5,
+        stagger: .1,
+    })
+
+    tl.to('.boundingelemDown', {
+        y: 0,
+        ease: Expo.easeInOut,
+        duration: 1.5,
+        delay: -1,
+        stagger: .1,
+    })
+
+    tl.from('#herobase', {
+        y: 10,
+        opacity: 0,
+        duration: 1.5,
+        delay: -1,
+        ease: Expo.easeInOut,
+    })
+})();
